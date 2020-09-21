7b8f21f297cfb5b05748be0f374f66ea7846649c
jeison-22 <jeisonfallas2109@gmail.com>
2020-09-21 10:00:38 -0600

    Agregado de clases del Business Logic Layer

---
+++ WebAnimal/BLL/ClsRollUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BLL
+{
+    public class ClsRollUsuario
+    {
+        //declaracion de Atributos
+        //Nesesarios en el manejo back-end de los usuarios
+        //respectivos set and get
+        public int Id { set; get; }
+        public string Descripcion { set; get; }
+    }
+}
+
---
+++ WebAnimal/BLL/ClsUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    class ClsUsuario
+    {
+        //declaracion de Atributos
+        //Nesesarios en el manejo back-end de los usuarios
+        //respectivos set and get
+        private int Id { set; get; }
+        private string Name { set; get; }
+        private string PassWord { set; get; }
+        private string Mail { set; get; }
+        private DateTime Time { set; get; }
+        private byte Estate { set; get; }
+        private int Roll { set; get; }
+
+    }
+}
